ca420a41c512ffc3191776c5d57a146bf6bdaff9
Niko185 <60343750+Niko185@users.noreply.github.com>
2023-02-04 22:30:54 +0500

    Develop preference

--- app/src/main/java/com/example/mynotepad/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/mynotepad/view/activities/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.mynotepad.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.mynotepad.R
 import com.example.mynotepad.databinding.ActivityMainBinding
 import com.example.mynotepad.fragments_operation.FragmentManager
 import com.example.mynotepad.fragments.NoteFragment
 import com.example.mynotepad.fragments.ShoppingListFragment
+import com.example.mynotepad.view.activities.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     private fun setBottomNavigationMenuListener() {
         binding.bottomNavigationMenu.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.settings -> Toast.makeText(applicationContext,"Settings click",Toast.LENGTH_SHORT).show()
+                R.id.settings -> startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
                 R.id.note -> FragmentManager.setFragment(NoteFragment.newInstance(), this@MainActivity)
                 R.id.shopping_list -> FragmentManager.setFragment(ShoppingListFragment.newInstance(), this@MainActivity)
                 R.id.add_item -> FragmentManager.openNowFragment?.onClickAdd()
---
+++ app/src/main/java/com/example/mynotepad/view/fragments/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.mynotepad.view.fragments
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.example.mynotepad.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preference, rootKey)
+    }
+}
