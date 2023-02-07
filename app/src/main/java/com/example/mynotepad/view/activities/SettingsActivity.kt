9761bc554c728c9e56cffb90dc34475f509e5cf1
Niko185 <60343750+Niko185@users.noreply.github.com>
2023-02-07 17:55:55 +0500

    Develop Application MyNotepad Done

--- app/src/main/java/com/example/mynotepad/view/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/mynotepad/view/activities/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mynotepad.view.activities
 
 import android.content.SharedPreferences
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
 import com.example.mynotepad.R
 import com.example.mynotepad.view.fragments.SettingsFragment
@@ -11,9 +13,16 @@ import com.example.mynotepad.view.fragments.SettingsFragment
 class SettingsActivity : AppCompatActivity() {
     private lateinit var pref: SharedPreferences
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         pref = PreferenceManager.getDefaultSharedPreferences(this)
-        setTheme(getSelectedTheme())
+        val themePref = if(pref.getString("key_theme", "Standard theme memory") == "Standard theme memory") R.style.Theme_MyNotepad  else R.style.Theme_MyNotepadTwo
+
+        if(themePref != R.style.Theme_MyNotepad) {
+            setTheme(resources.getIdentifier(themePref.toString(), "style", this.packageName))
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         if(savedInstanceState == null) {
@@ -22,15 +31,28 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private val listener = SharedPreferences.OnSharedPreferenceChangeListener { pref, key ->
+        if (key == "key_theme") {
+            recreate()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        pref.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onPause() {
+        pref.unregisterOnSharedPreferenceChangeListener(listener)
+        super.onPause()
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == android.R.id.home) {
             finish()
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    private fun getSelectedTheme(): Int {
-        return if(pref.getString("key_theme", "Standard theme memory") == "Standard theme memory") R.style.Theme_MyNotepad else R.style.Theme_MyNotepadTwo
     }
 
 }
